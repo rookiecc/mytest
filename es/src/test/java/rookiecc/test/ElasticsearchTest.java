80b514a8167a5f3f0757c633727d495d66596594
unknown <Administrator@WIN10-803050647.center.intranet.sinotrans>
2018-06-15 14:38:27 +0800

    add log4j

--- es/src/test/java/rookiecc/test/ElasticsearchTest.java
+++ es/src/test/java/rookiecc/test/ElasticsearchTest.java
@@ -3,6 +3,8 @@ package rookiecc.test;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
@@ -12,6 +14,7 @@ import org.junit.Test;
 import junit.framework.Assert;
 
 public class ElasticsearchTest {
+	public final static Log logger = LogFactory.getLog(ElasticsearchTest.class);
 	public final static String HOST = "127.0.0.1";
 	public final static int PORT = 9300;
 	
@@ -23,7 +26,7 @@ public class ElasticsearchTest {
 				.addTransportAddresses(new TransportAddress(InetAddress.getByName(HOST), PORT));
 
 		Assert.assertEquals("_client_", client.nodeName());
-
+		logger.info("testing...........");
 		// 关闭客户端
 		client.close();
 	}
